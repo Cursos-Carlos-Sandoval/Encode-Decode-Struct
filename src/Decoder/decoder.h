89f057f3341ccafc8217ee9a3059e68baa6471c9
Carlos Sandoval <csandovalc@unal.edu.co>
2023-02-15 23:07:48 -0500

    Add: Coder and Decoder docs

--- src/Decoder/decoder.h
+++ src/Decoder/decoder.h
@@ -1,7 +1,17 @@
 #ifndef DECODER
 #define DECODER
 
+/**
+ * Looks for a plain text file named 'filename' and processes it to print the encoded
+ * values
+ * @param filename
+ */
 void print_text(char filename[]);
+
+/**
+ * Looks for a binary file named 'filename' and processes it to print the encoded values
+ * @param filename
+ */
 void print_binary(char filename[]);
 
 #endif // DECODER
--- src/Encoder/encoder.h
+++ src/Encoder/encoder.h
@@ -3,7 +3,20 @@
 
 #include "../Person/person.h"
 
+/**
+ * Method in charge of converting each characteristic of an instance of the person
+ * structure into plain text and writing it to a file named 'filename'
+ * @param person
+ * @param filename
+ */
 void person_to_text(Person *person, char filename[]);
+
+/**
+ * Method in charge of converting each characteristic of an instance of the person
+ * structure into binary values and writing it to a file named 'filename'
+ * @param person
+ * @param filename
+ */
 void person_to_binary(Person *person, char filename[]);
 
 #endif // ENCODER
